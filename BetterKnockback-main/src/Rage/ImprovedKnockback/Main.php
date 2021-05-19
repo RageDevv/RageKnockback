8855af79011b934e15a2e653ef1522fc028f6331
Rage <84096429+RageDevv@users.noreply.github.com>
2021-05-19 19:58:47 -0400

    Add files via upload

---
+++ BetterKnockback-main/src/Rage/ImprovedKnockback/Main.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+namespace Rage\ImprovedKnockback;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\entity\{EntityDamageByEntityEvent, EntityDamageByChildEntityEvent};
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use Rage\ImprovedKnockback\Player as ModPlayer;
+class Main extends PluginBase implements Listener{
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    /**
+     * @priority MONITOR
+     * @ignoreCancelled true
+     */
+    public function onEntityDamageEventByEntity(EntityDamageByEntityEvent $event): void{
+        $damager = $event->getDamager();
+        if(!$event instanceof EntityDamageByChildEntityEvent and $damager->isSprinting()){
+            $event->setKnockback(1.3*$event->getKnockback());
+            if($damager instanceof Player){
+                $damager->toggleSprint(false);
+            }else{
+                $damager->setSprinting(false);
+            }
+        }
+    }
+    /**
+     * @priority MONITOR
+     */
+    public function onPlayerCreation(PlayerCreationEvent $event): void{
+        $event->setPlayerClass(ModPlayer::class);
+    }
+}
